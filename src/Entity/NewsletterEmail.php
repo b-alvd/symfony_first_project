2341437b00e3e02357a1eb685af37817e6dc0099
Basile <basilealeveque@gmail.com>
2024-06-05 11:21:25 +0200

    email required constraint

--- src/Entity/NewsletterEmail.php
+++ src/Entity/NewsletterEmail.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\NewsletterEmailRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: NewsletterEmailRepository::class)]
 class NewsletterEmail
@@ -12,8 +13,10 @@ class NewsletterEmail
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
+    
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "L'email est obligatoire")]
+    #[Assert\Email(message: "L'email {{ value }} n'est pas valide")]
     private ?string $email = null;
 
     public function getId(): ?int
